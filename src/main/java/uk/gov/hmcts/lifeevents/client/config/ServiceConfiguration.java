42be8e2f9605f32bff160388a02c35b191dc2432
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-04-15 11:36:57 +0100

    Temprorarily add logging

--- src/main/java/uk/gov/hmcts/lifeevents/client/config/ServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/lifeevents/client/config/ServiceConfiguration.java
@@ -7,12 +7,14 @@ import uk.gov.hmcts.lifeevents.client.service.NoOpDeathServiceImpl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @EnableConfigurationProperties
@@ -20,6 +22,9 @@ import org.springframework.context.annotation.Configuration;
 public class ServiceConfiguration {
 
   private static final Logger logger = LoggerFactory.getLogger(ServiceConfiguration.class);
+
+  @Autowired
+  Environment environment;
 
   private DeathApiClient deathApiClient;
 
@@ -30,12 +35,16 @@ public class ServiceConfiguration {
   @Bean
   @ConditionalOnProperty(name = "lev.ssl.publicCertificate")
   public DeathService deathService() {
+    logger.info("lev.ssl.publicCertificate" + environment.getProperty("lev.ssl.publicCertificate"));
+    logger.info("lev.bearertoken.clientId" + environment.getProperty("lev.bearertoken.clientId"));
     return new DeathServiceImpl(deathApiClient);
   }
 
   @Bean
   @ConditionalOnMissingBean(value = DeathService.class)
   public DeathService noOpDeathService() {
+    logger.info("lev.ssl.publicCertificate" + environment.getProperty("lev.ssl.publicCertificate"));
+    logger.info("lev.bearertoken.clientId" + environment.getProperty("lev.bearertoken.clientId"));
     return new NoOpDeathServiceImpl();
   }
 }
